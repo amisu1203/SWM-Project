2ba6ce3e4568ad6670842f63f8bd180263085811
jisoo <doh_0112@naver.com>
2023-03-30 01:31:10 +0900

    타이머 파일 추가

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 import certifi
 from bson.objectid import ObjectId
@@ -6,31 +8,31 @@ from flask import Flask, render_template, request, jsonify
 import hashlib
 
 app = Flask(__name__)
-
 ca = certifi.where()
 client = MongoClient(
-    'mongodb+srv://sparta:test@cluster0.g85ftsp.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+    "mongodb+srv://sparta:test@cluster0.g85ftsp.mongodb.net/?retryWrites=true&w=majority",
+    tlsCAFile=ca,
+)
 db = client.dbsparta
 
 
 # api
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 # sign-up apis
-
-@app.route('/join', methods=['GET', 'POST'])
+@app.route("/join", methods=["GET", "POST"])
 def join():
-    if request.method == 'POST':
-        id = request.form['id']
-        nickname = request.form['nickname']
-        password = request.form['password']
+    if request.method == "POST":
+        id = request.form["id"]
+        nickname = request.form["nickname"]
+        password = request.form["password"]
 
         #  아이디 중복 검사
-        if db.pjs.find_one({'id': id}):
-           return jsonify({'message': '이미 존재하는 아이디입니다.'})
+        if db.pjs.find_one({"id": id}):
+            return jsonify({"message": "이미 존재하는 아이디입니다."})
 
         # 랜덤 솔트 값 생성
         salt = os.urandom(16)
@@ -39,50 +41,78 @@ def join():
         hashed_password = hashlib.sha256(password.encode() + salt).hexdigest()
 
         doc = {
-            'id': id, 'nickname': nickname, 'salt': salt, 'password': hashed_password
-            }
+            "id": id,
+            "nickname": nickname,
+            "salt": salt,
+            "password": hashed_password,
+        }
         db.pjs.insert_one(doc)
-        
-        # return redirect(url_for('home'))
-        return jsonify({'message' :'가입 완료'})
-    else:
-        return render_template('join.html')
 
+        # return redirect(url_for('home'))
+        return jsonify({"message": "가입 완료"})
+    else:
+        return render_template("join.html")
+
+
+# timer apis
+@app.route("/timer", methods=["GET"])
+def timer_get():
+    all_timer = list(db.jjm.find({}, {"_id": False}))
+    return jsonify({"result": all_timer})
+
+
+@app.route("/attend", methods=["GET"])
+def attend_get():
+    all_attend = list(db.attend.find({}, {"_id": False}))
+    return jsonify({"result": all_attend})
+
+
+@app.route("/timer", methods=["POST"])
+def timer_post():
+    timer_receive = request.form["timer_give"]
+    doc = {"timer": timer_receive}
+    db.jjm.insert_one(doc)
+    return jsonify({"msg": "저장 완료!"})
+
+
+@app.route("/attend", methods=["POST"])
+def attend_post():
+    attend_receive = request.form["attend_give"]
+    doc = {"attend": attend_receive}
+    db.attend.insert_one(doc)
+    return jsonify({"msg": "출석 완료!"})
 
 
 # todo apis
 @app.route("/todo", methods=["POST"])
 def todo_post():
-    todo_receive = request.form['todo_give']
-    doc = {
-        'todo': todo_receive
-    }
+    todo_receive = request.form["todo_give"]
+    doc = {"todo": todo_receive}
     db.todolist.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+    return jsonify({"msg": "저장 완료!"})
 
 
 @app.route("/todo", methods=["GET"])
 def todo_get():
     comments = list(db.todolist.find())
-    comments = [{**comment, **{"_id": str(comment["_id"])}}
-                for comment in comments]
-    return jsonify({'result': comments})
-    
+    comments = [{**comment, **{"_id": str(comment["_id"])}} for comment in comments]
+    return jsonify({"result": comments})
 
 
 @app.route("/todo", methods=["DELETE"])
 def todo_delete():
     delete_receive = request.form["id"]
-    db.todolist.delete_one({'_id': ObjectId(delete_receive)})
-    return jsonify({'msg': "삭제 완료!"})
+    db.todolist.delete_one({"_id": ObjectId(delete_receive)})
+    return jsonify({"msg": "삭제 완료!"})
+
 
 @app.route("/todo", methods=["PUT"])
 def todo_update():
     id_receive = request.form["id"]
     new_todo = request.form["new_todo"]
-    db.todolist.update_one({'_id': ObjectId(id_receive)}, {"$set": {"todo": new_todo}})
-    return jsonify({'msg': "수정 완료!"})
+    db.todolist.update_one({"_id": ObjectId(id_receive)}, {"$set": {"todo": new_todo}})
+    return jsonify({"msg": "수정 완료!"})
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5001, debug=True)
---
+++ static/js/timer.js
@@ -0,0 +1,163 @@
+//timeSum 위치 바꾸면 큰일나요
+let timeSum = 0;
+
+//화면에 DB 반영
+$(document).ready(function () {
+  show_timer();
+  show_attend();
+  dailyAttend();
+});
+let temp_html;
+function show_timer() {
+  fetch("/timer")
+    .then((res) => res.json())
+    .then((data) => {
+      let rows = data["result"];
+      $("#todaystudy").empty();
+      rows.forEach((a) => {
+        let timer = a["timer"];
+        let b = parseInt(timer);
+        timeSum += b;
+        function getTimerString(timeSum) {
+          let hour = parseInt(timeSum / (60 * 60));
+          let min = parseInt((timeSum - hour * 60 * 60) / 60);
+          let sec = timeSum % 60;
+          return String(hour).padStart(2, "0") + ":" + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0");
+        }
+        return timeSum, (temp_html = `${getTimerString(timeSum)}`);
+      });
+
+      $("#todaystudy").append(temp_html);
+    });
+}
+
+//출첵 반영
+function show_attend() {
+  fetch("/attend")
+    .then((res) => res.json())
+    .then((data) => {
+      let rows = data["result"];
+      $("#attendcheck").empty();
+      rows.forEach((a) => {
+        let attend = a["attend"];
+        temp_html = `<li>${attend}</li>`;
+        $("#attendcheck").append(temp_html);
+      });
+    });
+}
+
+//출첵버튼 하루에 한번만 가능하게
+function dailyAttend() {
+  fetch("/attend")
+    .then((res) => res.json())
+    .then((data) => {
+      let rows = data["result"];
+      let date = new Date();
+      let attendDate = date.getFullYear() + "." + date.getMonth() + "." + date.getDate();
+      rows.forEach((a) => {
+        let attend = a["attend"];
+        if (attend === attendDate) {
+          btnDisabled();
+        } else {
+          btnActive();
+        }
+      });
+    });
+}
+
+//누적시간 전송
+function save_timer() {
+  let dayTimer = time;
+  let formData = new FormData();
+  console.log(time, typeof time);
+
+  formData.append("timer_give", dayTimer);
+
+  fetch("/timer", { method: "POST", body: formData })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data["msg"]);
+      window.location.reload();
+    });
+}
+
+//출첵
+function attendance() {
+  let date = new Date();
+  let attendance = date.getFullYear() + "." + date.getMonth() + "." + date.getDate();
+  let formData = new FormData();
+
+  formData.append("attend_give", attendance);
+
+  fetch("/attend", { method: "POST", body: formData })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data["msg"]);
+      window.location.reload();
+    });
+}
+
+//타이머 기본 세팅
+let timerState;
+let time = timeSum;
+let timer = document.getElementById("stopwatch");
+
+//타이머 출력
+function timerScreen() {
+  timer.innerText = timerString();
+  time++;
+}
+
+//타이머 시작
+function startTimer() {
+  timerScreen();
+  timerState = setTimeout(startTimer, 1000);
+}
+
+//타이머 중지
+function stopTimer() {
+  clearTimeout(timerState);
+}
+
+//타이머 초기화
+function resetTimer() {
+  stopTimer();
+  timer.innerText = "00:00:00";
+  time = 0;
+}
+
+//타이머 출력2
+function timerString() {
+  let hour = parseInt(time / (60 * 60));
+  let min = parseInt((time - hour * 60 * 60) / 60);
+  let sec = time % 60;
+
+  return String(hour).padStart(2, "0") + ":" + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0");
+}
+
+//출첵버튼 활성화
+function btnActive() {
+  const target = document.getElementById("attendance");
+  target.disabled = false;
+}
+
+//출첵버튼 비활성화
+function btnDisabled() {
+  const target = document.getElementById("attendance");
+  target.disabled = true;
+}
+
+//체크인,체크아웃 시간
+// function checkIn() {
+//   let today = new Date();
+//   alert(
+//     `체크인 시간 : ${today.getHours()}시 ${today.getMinutes()}분 ${today.getSeconds()}초`
+//   );
+// }
+
+// function checkOut() {
+//   let today = new Date();
+//   alert(
+//     `체크아웃 시간 : ${today.getHours()}시 ${today.getMinutes()}분 ${today.getSeconds()}초`
+//   );
+// }
